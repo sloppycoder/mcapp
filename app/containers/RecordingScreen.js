18b1abde97fd13ce0e14af86e270558e19273751
Neeraj Bhakuni <neeraj.bhakuni@tcs.com>
2018-07-28 12:50:04 +0530

    Audio recording and Playback functionality added

--- app/containers/RecordingScreen.js
+++ app/containers/RecordingScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Slider, Text, View } from 'react-native';
+import { Audio, FileSystem, Permissions } from 'expo';
+
 import MediaButton from '../components/MediaButton';
 
 import { styles } from '../styles';
@@ -16,33 +18,255 @@ class RecordingScreen extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { recorderStatus: PlayerStatus.PLAY, isRecording: true };
+    this.recording = null;
+    this.sound = null;
+    this.recordingSettings = JSON.parse(
+      JSON.stringify(Audio.RECORDING_OPTIONS_PRESET_LOW_QUALITY)
+    );
+    // this.recordingSettings.android['maxFileSize'] = 600000;
+    this.state = {
+      recorderStatus: PlayerStatus.PLAY,
+      displayRecording: true,
+      haveRecordingPermissions: false,
+      isRecording: false,
+      recordingDuration: null,
+      soundPosition: null,
+      soundDuration: null
+    };
   }
 
+  componentDidMount() {
+    this._askForPermissions();
+  }
+
+  componentWillUnmount() {
+    if (this.recording !== null) {
+      this._stopRecording();
+      this.recording.setOnRecordingStatusUpdate(null);
+      this.recording = null;
+    }
+    this._unloadPlayer();
+  }
+
+  // Private Methods
+  _askForPermissions = async () => {
+    // Recording Permission
+    const response = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+    this.setState({ haveRecordingPermissions: response.status === 'granted' });
+  };
+
+  _setUpRecording = async () => {
+    // Set Audio Mode
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: true,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX
+    });
+
+    const recording = new Audio.Recording();
+    await recording.prepareToRecordAsync(this.recordingSettings);
+    recording.setOnRecordingStatusUpdate(this._updateRecordingTimerStatus);
+
+    this.recording = recording;
+
+    this._startRecording();
+  };
+
+  _setUpPlayer = async () => {
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      playsInSilentLockedModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX
+    });
+    const { sound, status } = await this.recording.createNewLoadedSound(
+      {
+        isLooping: true,
+        isMuted: false,
+        volume: 1.0,
+        rate: 1.0,
+        shouldCorrectPitch: true
+      },
+      this._updateSoundTimerStatus
+    );
+    this.sound = sound;
+  };
+
+  _unloadPlayer = async () => {
+    if (this.sound !== null) {
+      try {
+        await this.sound.unloadAsync();
+      } catch (error) {
+        console.log('error', error);
+      }
+
+      this.sound.setOnPlaybackStatusUpdate(null);
+      this.sound = null;
+    }
+  };
+
+  _startRecording = async () => {
+    this.recording.setOnRecordingStatusUpdate(this._updateRecordingTimerStatus);
+    try {
+      await this.recording.startAsync();
+    } catch (error) {
+      // An error occurred!
+      console.log('error *****', error);
+    }
+  };
+
+  _pauseRecording = async () => {
+    try {
+      await this.recording.pauseAsync();
+    } catch (error) {
+      // Do nothing -- we are already unloaded.
+    }
+  };
+
+  _stopRecording = async () => {
+    try {
+      await this.recording.stopAndUnloadAsync();
+    } catch (error) {
+      // Do nothing -- we are already unloaded.
+    }
+    // const info = await FileSystem.getInfoAsync(this.recording.getURI());
+    // Setup Player
+    this._setUpPlayer();
+  };
+
+  _updateRecordingTimerStatus = status => {
+    if (status.canRecord) {
+      this.setState({
+        isRecording: status.isRecording,
+        recordingDuration: status.durationMillis
+      });
+    } else if (status.isDoneRecording) {
+      this.setState({
+        isRecording: false,
+        recordingDuration: status.durationMillis
+      });
+    }
+  };
+
+  _updateSoundTimerStatus = status => {
+    if (status.isLoaded) {
+      this.setState({
+        soundDuration: status.durationMillis,
+        soundPosition: status.positionMillis
+      });
+    } else {
+      this.setState({
+        soundDuration: null,
+        soundPosition: null
+      });
+      if (status.error) {
+        console.log(`FATAL PLAYER ERROR: ${status.error}`);
+      }
+    }
+  };
+
+  _getTimeFromMillis(millis) {
+    const totalSeconds = millis / 1000;
+    const seconds = Math.floor(totalSeconds % 60);
+    const minutes = Math.floor(totalSeconds / 60);
+
+    const padWithZero = number => {
+      const string = number.toString();
+      if (number < 10) {
+        return '0' + string;
+      }
+      return string;
+    };
+    return padWithZero(minutes) + ':' + padWithZero(seconds);
+  }
+
+  _getRecordingTimestamp() {
+    if (this.state.recordingDuration != null) {
+      return `${this._getTimeFromMillis(this.state.recordingDuration)}`;
+    }
+    return `${this._getTimeFromMillis(0)}`;
+  }
+
+  _getPlaybackTimestamp() {
+    if (
+      this.sound != null &&
+      this.state.soundPosition != null &&
+      this.state.soundDuration != null
+    ) {
+      return `${this._getTimeFromMillis(
+        this.state.soundPosition
+      )} / ${this._getTimeFromMillis(this.state.soundDuration)}`;
+    }
+    return '';
+  }
+
+  // Button Action
   _onRecordStartButton = () => {
     // Implement Audio Recording Start Logic
     this.setState({ recorderStatus: PlayerStatus.START });
+
+    // Setup and Start Recording
+    this._setUpRecording();
   };
 
   _onRecordPauseButton = () => {
     // Implement Audio Recording Pause Logic
+    if (this.state.displayRecording) {
+      // Audio Recoder Player
+      // Pause Recording
+      if (this.state.recorderStatus == PlayerStatus.START) {
+        // Pause Audio Recording
+        this._pauseRecording();
+      } else {
+        // Start Audio Recording
+        this._startRecording();
+      }
+    } else {
+      // Audio Media Player
+      if (this.sound != null) {
+        if (this.state.recorderStatus == PlayerStatus.START) {
+          this.sound.pauseAsync();
+        } else {
+          this.sound.playAsync();
+        }
+      }
+    }
+
     this.setState({
       recorderStatus:
         this.state.recorderStatus == PlayerStatus.START
           ? PlayerStatus.PAUSE
           : PlayerStatus.START
     });
-    if (this.state.isRecording) {
-      // Audio Recoder Player
-    } else {
-      // Audio Media Player
-    }
   };
 
   _onRecordStopButton = () => {
     // Implement Audio Recording Stop Logic
-    this.setState({ recorderStatus: PlayerStatus.STOP, isRecording: false });
+    this.setState({
+      recorderStatus: PlayerStatus.STOP,
+      displayRecording: false
+    });
+    this._stopRecording();
   };
+
+  _getSeekSliderPosition() {
+    if (
+      this.sound != null &&
+      this.state.soundPosition != null &&
+      this.state.soundDuration != null
+    ) {
+      return this.state.soundPosition / this.state.soundDuration;
+    }
+    return 0;
+  }
+
+  _onSeekSliderValueChange = value => {};
+
+  _onSeekSliderSlidingComplete = async value => {};
 
   render() {
     return (
@@ -56,26 +280,19 @@ class RecordingScreen extends React.Component {
           />
         ) : (
           <View style={[styles.flx1]}>
-            <View
-              style={[
-                styles.flx1,
-                styles.jcc,
-                styles.aic,
-                styles.flxc,
-                styles.jcsa
-              ]}
-            >
-              <Text style={[styles.tc, styles.b, styles.f1]}>{'01:00'}</Text>
-              {!this.state.isRecording ? (
+            <View style={[styles.flx1, styles.flxc, styles.aic, styles.jcsa]}>
+              <Text style={[styles.tc, styles.b, styles.f1]}>
+                {this.state.displayRecording
+                  ? this._getRecordingTimestamp()
+                  : this._getPlaybackTimestamp()}
+              </Text>
+              {!this.state.displayRecording ? (
                 <Slider
                   style={{ width: '80%' }}
-                  disabled={true}
-                  step={1}
-                  minimumValue={18}
-                  maximumValue={71}
-                  value={this.state.timer}
-                  onValueChange={val => this.setState({ timer: val })}
-                  onSlidingComplete={val => this.getVal(val)}
+                  disabled={false}
+                  value={this._getSeekSliderPosition()}
+                  onValueChange={this._onSeekSliderValueChange}
+                  onSlidingComplete={this._onSeekSliderSlidingComplete}
                 />
               ) : null}
             </View>
@@ -84,12 +301,12 @@ class RecordingScreen extends React.Component {
                 title={
                   this.state.recorderStatus == PlayerStatus.START
                     ? 'Pause'
-                    : this.state.isRecording
+                    : this.state.displayRecording
                       ? 'Record'
                       : 'Play'
                 }
                 styles={
-                  this.state.isRecording
+                  this.state.displayRecording
                     ? []
                     : [styles.flx1, styles.aic, styles.jcc]
                 }
@@ -100,7 +317,7 @@ class RecordingScreen extends React.Component {
                 ]}
                 onButtonPress={this._onRecordPauseButton}
               />
-              {this.state.isRecording ? (
+              {this.state.displayRecording ? (
                 <MediaButton
                   title={'Stop'}
                   buttonStyles={[styles.bgStop]}
